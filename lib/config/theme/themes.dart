ac14e683c8266bf75a927abe3d9977a4c06b6f2f
Fisayo <obilaja@gmail.com>
2023-05-24 21:52:34 +0100

    chore: configure color-scheme for darkTheme

--- lib/config/theme/themes.dart
+++ lib/config/theme/themes.dart
@@ -1,7 +1,20 @@
+import 'package:calentre/config/theme/colors.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  ThemeData lightTheme() {
-    return ThemeData();
+  static ThemeData darkTheme() {
+    return ThemeData(
+        colorScheme: ColorScheme(
+            brightness: Brightness.dark,
+            primary: AppColors.grey.s900,
+            onPrimary: AppColors.foundation.white,
+            secondary: AppColors.grey.s850,
+            onSecondary: AppColors.foundation.white,
+            error: AppColors.foundation.error,
+            onError: AppColors.foundation.white,
+            background: AppColors.grey.s950,
+            onBackground: AppColors.foundation.white,
+            surface: AppColors.grey.s900,
+            onSurface: AppColors.foundation.white));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:calentre/config/theme/themes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      themeMode: ThemeMode.light,
+      themeMode: ThemeMode.dark,
+      darkTheme: AppTheme.darkTheme(),
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
