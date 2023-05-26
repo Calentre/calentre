7dead96dd60f25701093af0f1bcf6bab514bbcdd
Fisayo <obilaja@gmail.com>
2023-05-26 20:34:58 +0100

    chore: add icon and improve button style

--- lib/shared/button.dart
+++ lib/shared/button.dart
@@ -1,17 +1,39 @@
+import 'package:calentre/config/extensions/spacing.dart';
 import 'package:calentre/config/theme/button_style.dart';
 import 'package:flutter/material.dart';
 
 class AppButton extends StatelessWidget {
-  const AppButton({super.key, required this.title});
+  const AppButton({
+    super.key,
+    required this.title,
+    this.icon,
+  });
+
   final String title;
+  final Widget? icon;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-        style: buttonStyle(context),
-        onPressed: () {
-          print("The button was pressed");
-        },
-        child: SizedBox(width: 500, child: Center(child: Text(title))));
+      style: buttonStyle(context),
+      onPressed: () {
+        print("The button was pressed");
+      },
+      child: SizedBox(
+        height: 50,
+        width: 500,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 30,
+              child: icon,
+            ),
+            const SizedBox().x10(),
+            Text(title),
+          ],
+        ),
+      ),
+    );
   }
 }
