812f397779fecb8f7c17f8cb24c54768007f49af
Fisayo <obilaja@gmail.com>
2023-05-27 14:18:28 +0100

    chore: add calentreHome

---
+++ lib/app/calentreHome/calentre_home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CalentreHome extends StatelessWidget {
+  const CalentreHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Calentre Home"),
+      ),
+    );
+  }
+}
