c25f5ad9730b0dde0cc7e95fd48c2073f325cc17
Fisayo <obilaja@gmail.com>
2023-06-01 19:49:49 +0100

    chore: configure payment card UI

--- lib/app/payments/presentation/widgets/payment_card.dart
+++ lib/app/payments/presentation/widgets/payment_card.dart
@@ -13,71 +13,14 @@ class PaymentCard extends StatelessWidget {
       return BorderCard(
         verticalPadding: 24,
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12),
+          padding: EdgeInsets.symmetric(horizontal: 12),
           child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Container(
-                constraints: const BoxConstraints(
-                  minWidth: 96,
-                  maxWidth: 118,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Column(
-                      children: [
-                        Text(
-                          "2023",
-                          style: TextStyle(color: AppColors.grey.s500),
-                        ),
-                        const SizedBox().y4(),
-                        Container(
-                          constraints: const BoxConstraints(
-                            minWidth: 96,
-                            maxWidth: 108,
-                          ),
-                          child: Text(
-                            "30th",
-                            style: Theme.of(context).textTheme.titleLarge,
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                        const SizedBox().y4(),
-                        Text(
-                          "Sept",
-                          style: TextStyle(color: AppColors.grey.s500),
-                        ),
-                      ],
-                    ),
-                    Container(
-                        height: 70,
-                        width: 1,
-                        decoration: BoxDecoration(
-                          color: AppColors.grey.s700,
-                        )),
-                  ],
-                ),
-              ),
-              const SizedBox().x10(),
-              Expanded(
-                child: SizedBox(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Wola Kin",
-                        style: Theme.of(context).textTheme.titleLarge,
-                      ),
-                      const Text("30mins - Life Coaching and meeting"),
-                      const Text("WolaKin@gmail.com")
-                    ],
-                  ),
-                ),
-              ),
               Row(
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                  const FaIcon(FontAwesomeIcons.paypal),
+                  FaIcon(FontAwesomeIcons.paypal),
                   const SizedBox().x10(),
                   Text(
                     "\$5",
@@ -85,7 +28,25 @@ class PaymentCard extends StatelessWidget {
                   ),
                   const SizedBox().x10(),
                 ],
-              )
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.circle,
+                    color: AppColors.foundation.error,
+                  ),
+                  Switch(
+                    // This bool value toggles the switch.
+                    value: false,
+                    activeColor: Colors.red,
+                    onChanged: (bool value) {
+                      // This is called when the user toggles the switch.
+                    },
+                  ),
+                  const FaIcon(FontAwesomeIcons.trash)
+                ],
+              ),
             ],
           ),
         ),
