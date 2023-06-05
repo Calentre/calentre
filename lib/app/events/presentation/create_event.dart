88b40d8ecabb7949b85c1e2e7463586fd3c175eb
Fisayo <obilaja@gmail.com>
2023-06-05 09:25:34 +0100

    chore: add create event widget

---
+++ lib/app/events/presentation/create_event.dart
@@ -0,0 +1,36 @@
+import 'package:calentre/config/constraints/constraints.dart';
+import 'package:calentre/config/theme/colors.dart';
+import 'package:flutter/material.dart';
+
+class CreateEventView extends StatelessWidget {
+  const CreateEventView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          constraints: BoxConstraints(maxWidth: WebConstraints.maxWidth),
+          child: Column(
+            children: [
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [Text("create Event"), Text("Save")],
+              ),
+              Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: AppColors.grey.s700,
+                        width: 1.0,
+                      ),
+                    ),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
