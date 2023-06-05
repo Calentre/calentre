bd535a4f773e474260d10b5f14fb2104a0d861b4
Fisayo <obilaja@gmail.com>
2023-06-05 15:04:14 +0100

    chore: name, event, video call and other form fields

--- lib/app/create_events/presentation/widget/form_fields.dart
+++ lib/app/create_events/presentation/widget/form_fields.dart
@@ -1,3 +1,4 @@
+import 'package:calentre/config/extensions/spacing.dart';
 import 'package:calentre/config/theme/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -9,19 +10,113 @@ class CreateEventFormFields extends StatelessWidget {
     return Column(
       children: [
         Form(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text("Enter Event Name"),
-            TextFormField(
-              decoration: const InputDecoration(
-                hintText: "Event Name",
-                filled: true,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Event Name",
+                    style: Theme.of(context)
+                        .textTheme
+                        .labelLarge!
+                        .copyWith(fontWeight: FontWeight.normal),
+                  ),
+                  const SizedBox().y10(),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Enter Event Name",
+                      filled: true,
+                    ),
+                    cursorColor: AppColors.foundation.white,
+                  ),
+                ],
               ),
-              cursorColor: AppColors.foundation.white,
-            )
-          ],
-        ))
+              const SizedBox().y20(),
+              const SizedBox().y10(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Description (Optional)",
+                    style: Theme.of(context)
+                        .textTheme
+                        .labelLarge!
+                        .copyWith(fontWeight: FontWeight.w100),
+                  ),
+                  const SizedBox().y10(),
+                  TextFormField(
+                    maxLines: 5,
+                    decoration: const InputDecoration(
+                      hintText: "What's your event about?",
+                      filled: true,
+                    ),
+                    cursorColor: AppColors.foundation.white,
+                  ),
+                ],
+              ),
+              const SizedBox().y20(),
+              const SizedBox().y10(),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Video Call type",
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelLarge!
+                                .copyWith(fontWeight: FontWeight.w100),
+                          ),
+                          const SizedBox().y10(),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              hintText: "Select",
+                              filled: true,
+                            ),
+                            cursorColor: AppColors.foundation.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox().x10(),
+                  Expanded(
+                    child: SizedBox(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Duration",
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelLarge!
+                                .copyWith(fontWeight: FontWeight.w100),
+                          ),
+                          const SizedBox().y10(),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              hintText: "Select",
+                              filled: true,
+                            ),
+                            cursorColor: AppColors.foundation.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
