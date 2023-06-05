b85eec48de73cb5eb90b4ea85ed2fc5bfdbee08c
Fisayo <obilaja@gmail.com>
2023-06-05 10:12:11 +0100

    chore: create event appBar created.

--- lib/app/events/presentation/create_event.dart
+++ lib/app/events/presentation/create_event.dart
@@ -1,6 +1,10 @@
+import 'package:calentre/config/constraints/constraints.dart';
+import 'package:calentre/config/extensions/spacing.dart';
 import 'package:calentre/config/theme/colors.dart';
+import 'package:calentre/shared/button.dart';
 import 'package:calentre/shared/navbar.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CreateEventView extends StatelessWidget {
   const CreateEventView({super.key});
@@ -9,14 +13,33 @@ class CreateEventView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Container(
+        child: SizedBox(
           // constraints: BoxConstraints(maxWidth: WebConstraints.maxWidth),
           child: Column(
             children: [
               const NavBar(),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [Text("create Event"), Text("Save")],
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 24),
+                width: WebConstraints.maxWidth,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const FaIcon(FontAwesomeIcons.circleArrowLeft),
+                        const SizedBox().x20(),
+                        Text("Create Event",
+                            style: Theme.of(context).textTheme.headlineSmall),
+                      ],
+                    ),
+                    AppButton(
+                      title: "Create",
+                      onPressed: () {},
+                      gradient: true,
+                      width: 100,
+                    )
+                  ],
+                ),
               ),
               Container(
                   width: MediaQuery.of(context).size.width,
