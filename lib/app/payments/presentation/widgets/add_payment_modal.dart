369b9333ee8fc037f9bd87894cdc2a1d05a80197
Fisayo <obilaja@gmail.com>
2023-07-24 19:58:35 +0100

    chore: extract function into widget

--- lib/app/payments/presentation/widgets/add_payment_modal.dart
+++ lib/app/payments/presentation/widgets/add_payment_modal.dart
@@ -1,5 +1,8 @@
+import 'package:calentre/config/extensions/spacing.dart';
 import 'package:calentre/config/theme/colors.dart';
+import 'package:calentre/shared/border_card.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 Future<String?> addPaymentModal(BuildContext context) {
   return showDialog<String>(
@@ -9,9 +12,33 @@ Future<String?> addPaymentModal(BuildContext context) {
       shape: RoundedRectangleBorder(
           borderRadius: const BorderRadius.all(Radius.circular(10)),
           side: BorderSide(color: AppColors.grey.s700, width: 1)),
-      title: const Text('AlertDialog Title'),
-      content:
-          Container(width: 400, child: const Text('AlertDialog description')),
+      content: Container(
+          padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 12),
+          width: 400,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text("Add a New Payment"),
+              Container(
+                padding: const EdgeInsets.all(10),
+                margin: const EdgeInsets.symmetric(vertical: 12),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: AppColors.accent.purpleMute,
+                ),
+                child: Text(
+                  "We only support direct transfers for all payment methods at the moment.",
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelSmall!
+                      .copyWith(color: AppColors.grey.s950),
+                ),
+              ),
+              paymentType(),
+              paymentType(),
+              paymentType(),
+            ],
+          )),
       actions: <Widget>[
         TextButton(
           onPressed: () => Navigator.pop(context, 'Cancel'),
@@ -22,6 +49,38 @@ Future<String?> addPaymentModal(BuildContext context) {
           child: const Text('OK'),
         ),
       ],
+    ),
+  );
+}
+
+Container paymentType() {
+  return Container(
+    margin: const EdgeInsets.symmetric(vertical: 4),
+    child: Material(
+      borderRadius: BorderRadius.circular(10),
+      color: AppColors.grey.s850,
+      child: InkWell(
+        onTap: () {},
+        borderRadius: BorderRadius.circular(10),
+        child: BorderCard(
+            width: double.maxFinite,
+            verticalPadding: 10,
+            horizontalPadding: 12,
+            color: AppColors.grey.s850.withOpacity(0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    const FaIcon(FontAwesomeIcons.building),
+                    const SizedBox().x10(),
+                    const Text("Brank Transfer")
+                  ],
+                ),
+                const FaIcon(FontAwesomeIcons.circleArrowRight),
+              ],
+            )),
+      ),
     ),
   );
 }
