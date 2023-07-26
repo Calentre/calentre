9a6e8ccced60312b22690e220c6e2dcc82138faa
Fisayo <obilaja@gmail.com>
2023-07-26 20:23:38 +0100

    chore: add firebase to project

---
+++ lib/config/firebase_options.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+
+FirebaseOptions firebaseOptions() {
+  return const FirebaseOptions(
+    apiKey: "AIzaSyCuRVHqH7HPKPVv8wf_eOEVIQY1dXoVIOM",
+    authDomain: "calentre-bb514.firebaseapp.com",
+    projectId: "calentre-bb514",
+    storageBucket: "calentre-bb514.appspot.com",
+    messagingSenderId: "45667816678",
+    appId: "1:45667816678:web:6e10090547d1b745ce1727",
+    measurementId: "G-WKLHK46V3Q",
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:calentre/app/home/presentation/bloc/home_bloc.dart';
+import 'package:calentre/config/firebase_options.dart';
 import 'package:calentre/config/routes/routes.dart';
 import 'package:calentre/config/theme/themes.dart';
 import 'package:feature_notifier/feature_notifier.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: firebaseOptions());
   await FeatureNotifier.init();
   FeatureNotifier.persistAll();
   usePathUrlStrategy();
