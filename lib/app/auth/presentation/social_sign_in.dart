78e2b0c3a82eae85ab32d9b2c2b08b5a9b60c59a
Fisayo <obilaja@gmail.com>
2023-07-27 20:32:18 +0100

    chore: add gradient background to login

--- lib/app/auth/presentation/social_sign_in.dart
+++ lib/app/auth/presentation/social_sign_in.dart
@@ -1,3 +1,4 @@
+import 'package:animate_gradient/animate_gradient.dart';
 import 'package:calentre/config/extensions/spacing.dart';
 import 'package:calentre/config/routes/routes.dart';
 import 'package:calentre/config/theme/colors.dart';
@@ -14,66 +15,83 @@ class SocialSignIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BorderCard(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        iconFramer(imageTitle: 'logo.svg'),
-                        const SizedBox().x10(),
-                        Text(
-                          "Calentre",
-                          style: Theme.of(context)
-                              .textTheme
-                              .displaySmall!
-                              .copyWith(fontWeight: FontWeight.w900),
-                        ),
-                      ],
-                    ),
-                    const SizedBox().y20(),
-                    Text(
-                      "Continue to Login/Signup",
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                    const SizedBox().y20(),
-                    const SizedBox().y20(),
-                    AppButton(
-                      title: "Login with Google",
-                      icon: iconFramer(
-                        imageTitle: 'google.png',
+      body: AnimateGradient(
+        duration: const Duration(seconds: 5),
+        primaryBegin: Alignment.topLeft,
+        primaryEnd: Alignment.bottomLeft,
+        secondaryBegin: Alignment.bottomLeft,
+        secondaryEnd: Alignment.topRight,
+        primaryColors: [
+          AppColors.grey.s950,
+          AppColors.gradient.g031.withOpacity(.2),
+          AppColors.grey.s950,
+        ],
+        secondaryColors: [
+          AppColors.grey.s950,
+          AppColors.gradient.g022.withOpacity(.2),
+          AppColors.grey.s950,
+        ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              BorderCard(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          iconFramer(imageTitle: 'logo.svg'),
+                          const SizedBox().x10(),
+                          Text(
+                            "Calentre",
+                            style: Theme.of(context)
+                                .textTheme
+                                .displaySmall!
+                                .copyWith(fontWeight: FontWeight.w900),
+                          ),
+                        ],
                       ),
-                      onPressed: () async {
-                        // context.goNamed(AppRoutes.calentreHome);
-                        await signInWithGoogle();
-                      },
-                    ),
-                    const SizedBox().y10(),
-                    AppButton(
-                      title: "Other Options are coming soon",
-                      icon: iconFramer(imageTitle: 'slack.png'),
-                      onPressed: () {},
-                    ),
-                  ],
+                      const SizedBox().y20(),
+                      Text(
+                        "Continue to Login/Signup",
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      const SizedBox().y20(),
+                      const SizedBox().y20(),
+                      AppButton(
+                        title: "Login with Google",
+                        icon: iconFramer(
+                          imageTitle: 'google.png',
+                        ),
+                        onPressed: () async {
+                          context.goNamed(AppRoutes.calentreHome);
+                          // await signInWithGoogle();
+                        },
+                      ),
+                      const SizedBox().y10(),
+                      AppButton(
+                        title: "Other Options are coming soon",
+                        icon: iconFramer(imageTitle: 'slack.png'),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const SizedBox().y20(),
-            const SizedBox().y20(),
-            Text(
-              "By signing up, you agree to our Privacy Policy and Terms of Use.",
-              style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                    color: AppColors.grey.s500,
-                  ),
-            )
-          ],
+              const SizedBox().y20(),
+              const SizedBox().y20(),
+              Text(
+                "By signing up, you agree to our Privacy Policy and Terms of Use.",
+                style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                      color: AppColors.grey.s500,
+                    ),
+              )
+            ],
+          ),
         ),
       ),
     );
