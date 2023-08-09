cdfd9798ab41b8ed64fb08aff6b200cb88d1b638
Fisayo <obilaja@gmail.com>
2023-08-09 21:02:22 +0100

    chore: authRepo concrete implementation complete

--- lib/features/auth/data/repository/auth_repository_impl.dart
+++ lib/features/auth/data/repository/auth_repository_impl.dart
@@ -1,21 +1,32 @@
 import 'package:calentre/core/resources.dart';
+import 'package:calentre/features/auth/data/data_sources/auth_service.dart';
 import 'package:calentre/features/auth/data/models/user_model.dart';
 import 'package:calentre/features/auth/domain/repository/auth_respository.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
-  // AuthRepositoryImpl(this.);
+  AuthRepositoryImpl(this._authService);
+
+  final AuthService _authService;
   @override
   Future<DataState> signInWithEmail() {
     throw UnimplementedError();
   }
 
   @override
-  Future<DataState<CalentreUser>> signInWithGoogle() {
-    //Get useremail
+  Future<DataState<CalentreUser, Exception>> signInWithGoogle() async {
+    try {
+      final signInResponse = await _authService.signInWithGoogle();
 
-    //if user !exist, we signup user w/ POST data
-    //return user Data
-    throw UnimplementedError();
+      if (signInResponse != null) {
+        return DataSuccess(signInResponse);
+      } else {
+        return DataFailure(
+            const AuthException("couldn't signin this google user"));
+      }
+    } on Exception catch (e) {
+      return DataFailure(e);
+    }
   }
 
   @override
--- lib/features/auth/domain/repository/auth_respository.dart
+++ lib/features/auth/domain/repository/auth_respository.dart
@@ -4,7 +4,7 @@ import 'package:calentre/features/auth/data/models/user_model.dart';
 ///Interface for all sign-in/up methods.
 abstract class AuthRepository {
   //define a custom user response after signin
-  Future<DataState<CalentreUser>> signInWithGoogle();
+  Future<DataState<CalentreUser, Exception>> signInWithGoogle();
 
   Future<DataState> signInWithEmail();
 
