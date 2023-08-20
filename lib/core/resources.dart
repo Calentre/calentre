c2d4e21ceccdb2cdb021d3171f95c26a57edf5cd
Fisayo <obilaja@gmail.com>
2023-08-20 20:09:16 +0100

    test: unit test Auth Data Layer

--- lib/core/resources.dart
+++ lib/core/resources.dart
@@ -11,8 +11,33 @@ abstract class DataState<T, E> {
 
 class DataSuccess<T, E> extends DataState<T, E> {
   DataSuccess(T data) : super(data: data);
+
+//These overides are for testing purposes, and allow us compare the same instance of
+//this class when testing.
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is DataSuccess &&
+          runtimeType == other.runtimeType &&
+          data == other.data;
+
+  @override
+  int get hashCode => data.hashCode;
 }
 
 class DataFailure<T, E> extends DataState<T, E> {
-  DataFailure(E? exception) : super(exception: exception);
+  DataFailure(E exception) : super(exception: exception);
+
+//These overides are for testing purposes, and allow us compare the same instance of
+//this class when testing.
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is DataFailure &&
+          runtimeType == other.runtimeType &&
+          exception == other.exception;
+
+  @override
+  int get hashCode => exception.hashCode;
 }
---
+++ test/app/auth/data/reporsitory/auth_repo_impl_test.dart
@@ -0,0 +1,42 @@
+import 'package:calentre/core/resources.dart';
+import 'package:calentre/features/auth/data/data_sources/auth_service.dart';
+import 'package:calentre/features/auth/data/repository/auth_repository_impl.dart';
+import 'package:calentre/features/auth/domain/repository/auth_respository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+//Mock AuthImplementation repo
+class AuthServiceMock extends Mock implements AuthService {
+  @override
+  Future<bool> signInWithGoogle() async {
+    return authResponse;
+  }
+}
+
+bool authResponse = false;
+
+void main() {
+  final AuthServiceMock authServiceMock = AuthServiceMock();
+  final AuthRepository authRepository = AuthRepositoryImpl(authServiceMock);
+  group('Auth Repository Implementation Test ', () {
+    test('//SignInWithGoogle() function Should return DataSuccess(true)',
+        () async {
+      authResponse = true;
+      final response = await authRepository.signInWithGoogle();
+
+      expect(response, DataSuccess<bool, Exception>(true));
+    });
+    test('//SignInWithGoogle() function Should return DataFailure(Exception())',
+        () async {
+      authResponse = false;
+      final response = await authRepository.signInWithGoogle();
+
+      expect(
+          response,
+          DataFailure(const AuthException(
+              "Unable to sign this user in : CODE 1",
+              statusCode: '500')));
+    });
+  });
+}
