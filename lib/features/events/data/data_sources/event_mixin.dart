9f291e0c5248bb851fdd550e4fda6eb6909ad193
Fisayo <obilaja@gmail.com>
2023-08-24 00:01:38 +0100

    chore: add createEvent service

--- lib/features/events/data/data_sources/event_mixin.dart
+++ lib/features/events/data/data_sources/event_mixin.dart
@@ -1,6 +1,6 @@
 mixin EventMixin {
   Future<dynamic> getEvent();
-  Future<dynamic> createEvent();
+  Future<dynamic> createEvent(dynamic param);
   Future<dynamic> deleteEvent();
   Future<dynamic> updateEvent();
 }
