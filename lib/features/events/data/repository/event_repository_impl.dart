7a59e7e686d8be0990b2f77407c59df9cf21b6a4
Fisayo <obilaja@gmail.com>
2023-08-25 11:51:32 +0100

    chore: create event data layer configureation complete

---
+++ lib/features/events/data/repository/event_repository_impl.dart
@@ -0,0 +1,37 @@
+import 'package:calentre/core/resources.dart';
+import 'package:calentre/features/events/data/data_sources/event_service.dart';
+import 'package:calentre/features/events/domain/reporsitory/event_repository.dart';
+
+class EventRepositoryImpl implements EventRepository {
+  EventRepositoryImpl(this._eventService);
+
+  final EventService _eventService;
+  @override
+  Future<DataState<bool, Exception>> createEvent(
+      Map<String, dynamic> param) async {
+    try {
+      final res = await _eventService.createEvent(param);
+      return DataSuccess(res);
+    } catch (e) {
+      return DataFailure(Exception(e));
+    }
+  }
+
+  @override
+  Future deleteEvent() {
+    // TODO: implement deleteEvent
+    throw UnimplementedError();
+  }
+
+  @override
+  Future getEvent() {
+    // TODO: implement getEvent
+    throw UnimplementedError();
+  }
+
+  @override
+  Future updateEvent() {
+    // TODO: implement updateEvent
+    throw UnimplementedError();
+  }
+}
