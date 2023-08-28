6456edfdc841f719c5611a2dacfeb6e61ef95e3a
Fisayo <obilaja@gmail.com>
2023-08-28 20:53:48 +0100

    chore: register create event usecase

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -5,6 +5,10 @@ import 'package:calentre/features/auth/data/repository/auth_repository_impl.dart
 import 'package:calentre/features/auth/domain/repository/auth_respository.dart';
 import 'package:calentre/features/auth/domain/usescases/sign_in_with_google.dart';
 import 'package:calentre/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:calentre/features/events/data/data_sources/event_service.dart';
+import 'package:calentre/features/events/data/repository/event_repository_impl.dart';
+import 'package:calentre/features/events/domain/reporsitory/event_repository.dart';
+import 'package:calentre/features/events/domain/usecases/event_usecase.dart';
 import 'package:calentre/utils/initializers.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
@@ -15,7 +19,7 @@ import 'features/auth/data/models/user_model.dart';
 final sl = GetIt.instance;
 
 Future<void> initializeDependencies() async {
-  // Singletons
+  //Auth Singletons
   sl.registerSingleton<Dio>(Dio());
   sl.registerSingleton<UserDTO>(UserDTO(email: ''));
   sl.registerSingleton<RemoteURLs>(RemoteURLs());
@@ -27,6 +31,11 @@ Future<void> initializeDependencies() async {
   sl.registerSingleton<SignInWithGoogleUseCase>(SignInWithGoogleUseCase(sl()));
   sl.registerSingleton<CalentreUser>(
       const CalentreUser(userId: '', name: '', email: ''));
+
+  //Events Singletons
+  sl.registerSingleton<EventService>(EventService(sl()));
+  sl.registerSingleton<EventRepository>(EventRepositoryImpl(sl()));
+  sl.registerSingleton<CreateEventUsesCase>(CreateEventUsesCase(sl()));
 
   //Factory
   sl.registerFactory<AuthBloc>(() => AuthBloc(sl()));
