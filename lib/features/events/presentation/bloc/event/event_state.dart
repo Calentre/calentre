faf05089ae052ff2fbc04af9676244f5892717a4
Fisayo <obilaja@gmail.com>
2023-08-30 10:12:28 +0100

    chore: configure calentreEventState

---
+++ lib/features/events/presentation/bloc/event/event_state.dart
@@ -0,0 +1,21 @@
+import 'package:calentre/features/events/data/models/calentre_event.dart';
+import 'package:equatable/equatable.dart';
+
+class CalentreEventState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class CalentreEventStateInitialState extends CalentreEventState {
+  final CalentreEvent calentreEvent = const CalentreEvent();
+  @override
+  List<Object?> get props => [calentreEvent];
+}
+
+class CalentreEventUpdatedState extends CalentreEventState {
+  CalentreEventUpdatedState({required this.calentreEvent});
+  final CalentreEvent calentreEvent;
+
+  @override
+  List<Object?> get props => [calentreEvent];
+}
