063ab2a957d3becef8540a3c10342395ed16325c
Fisayo <obilaja@gmail.com>
2024-03-17 10:07:29 +0000

    chore: update Calentre event details event

--- lib/features/events/presentation/bloc/event/event_event.dart
+++ lib/features/events/presentation/bloc/event/event_event.dart
@@ -1,3 +1,28 @@
+import 'package:calentre/features/events/data/models/calentre_event.dart';
+
 class CalentreEventEvent {}
 
 class ProceedToSetAvailabilityEvent extends CalentreEventEvent {}
+
+class UpdateEventDetails extends CalentreEventEvent {
+  String? amount;
+  String? duration;
+  String? eventDescription;
+  String? eventLink;
+  String? eventName;
+  String? eventType;
+  String? isMultiple;
+  String? videoCallType;
+  Days? days;
+
+  UpdateEventDetails(
+      {this.amount,
+      this.duration,
+      this.eventDescription,
+      this.eventLink,
+      this.eventName,
+      this.eventType,
+      this.isMultiple,
+      this.videoCallType,
+      this.days});
+}
