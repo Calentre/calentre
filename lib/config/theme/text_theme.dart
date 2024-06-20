307711a095d38a37012d9b3a7ca6d98ae39ea695
Fisayo <obilaja@gmail.com>
2024-06-20 14:40:49 +0100

    chore: update font

--- lib/config/theme/text_theme.dart
+++ lib/config/theme/text_theme.dart
@@ -3,80 +3,80 @@ import 'package:google_fonts/google_fonts.dart';
 
 TextTheme textTheme() {
   return TextTheme(
-    displayLarge: GoogleFonts.inter(
+    displayLarge: GoogleFonts.workSans(
       fontWeight: FontWeight.w400,
       fontSize: 57,
       height: 64 / 57,
       letterSpacing: -0.25,
     ),
-    displayMedium: GoogleFonts.inter(
+    displayMedium: GoogleFonts.workSans(
       fontWeight: FontWeight.w400,
       fontSize: 45,
       height: 52 / 45,
     ),
-    displaySmall: GoogleFonts.inter(
+    displaySmall: GoogleFonts.workSans(
       fontWeight: FontWeight.w400,
       fontSize: 36,
       height: 44 / 36,
     ),
-    headlineLarge: GoogleFonts.inter(
+    headlineLarge: GoogleFonts.workSans(
       fontWeight: FontWeight.w400,
       fontSize: 32,
       height: 40 / 32,
     ),
-    headlineMedium: GoogleFonts.inter(
+    headlineMedium: GoogleFonts.workSans(
       fontWeight: FontWeight.w400,
       fontSize: 28,
       height: 36 / 28,
     ),
-    headlineSmall: GoogleFonts.inter(
+    headlineSmall: GoogleFonts.workSans(
       fontWeight: FontWeight.w400,
       fontSize: 24,
       height: 32 / 24,
     ),
-    titleLarge: GoogleFonts.inter(
+    titleLarge: GoogleFonts.workSans(
       fontWeight: FontWeight.w700,
       fontSize: 22,
       height: 28 / 22,
     ),
-    titleMedium: GoogleFonts.inter(
+    titleMedium: GoogleFonts.workSans(
       fontWeight: FontWeight.w600,
       fontSize: 16,
       height: 24 / 16,
       letterSpacing: 0.1,
     ),
-    titleSmall: GoogleFonts.inter(
+    titleSmall: GoogleFonts.workSans(
       fontWeight: FontWeight.w600,
       fontSize: 14,
       height: 20 / 14,
       letterSpacing: 0.1,
     ),
-    labelLarge: GoogleFonts.inter(
+    labelLarge: GoogleFonts.workSans(
       fontWeight: FontWeight.w700,
       fontSize: 14,
       height: 20 / 14,
     ),
-    labelMedium: GoogleFonts.inter(
+    labelMedium: GoogleFonts.workSans(
       fontWeight: FontWeight.w700,
       fontSize: 12,
       height: 16 / 12,
     ),
-    labelSmall: GoogleFonts.inter(
+    labelSmall: GoogleFonts.workSans(
       fontWeight: FontWeight.w700,
       fontSize: 11,
       height: 16 / 11,
     ),
-    bodyLarge: GoogleFonts.inter(
+    bodyLarge: GoogleFonts.workSans(
       fontWeight: FontWeight.w400,
       fontSize: 16,
       height: 24 / 16,
     ),
-    bodyMedium: GoogleFonts.inter(
+    bodyMedium: GoogleFonts.workSans(
       fontWeight: FontWeight.w400,
       fontSize: 14,
       height: 20 / 14,
     ),
-    bodySmall: GoogleFonts.inter(
+    bodySmall: GoogleFonts.workSans(
       fontWeight: FontWeight.w400,
       fontSize: 12,
       height: 16 / 12,
